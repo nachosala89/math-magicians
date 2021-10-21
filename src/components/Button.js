c123acace937108577ac8cb84baf76eff9058bd3
nachosala <nachosala89@gmail.com>
2021-10-21 00:09:17 -0300

    Add events handlers to Calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClickButton = this.handleClickButton.bind(this);
+  }
+
+  handleClickButton() {
+    const { input, handleClickButton, ...state } = this.props;
+    handleClickButton(calculate(state, input));
+  }
+
   render() {
     const { input, btnClass } = this.props;
     const classes = `calc-btn col-md-1 py-3 ${btnClass}`;
     return (
-      <button className={classes} type="button">{input}</button>
+      <button className={classes} type="button" onClick={this.handleClickButton}>{input}</button>
     );
   }
 }
 
 Button.propTypes = {
-  input: PropTypes.isRequired,
+  input: PropTypes.string.isRequired,
   btnClass: PropTypes.string.isRequired,
+  handleClickButton: PropTypes.func.isRequired,
 };
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,63 @@
 import React from 'react';
 import Button from './Button';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClickButton = this.handleClickButton.bind(this);
+  }
+
+  handleClickButton(obj) {
+    console.log(obj);
+    this.setState(obj);
+  }
+
   render() {
+    const { total, next, operation } = this.state;
+    let result = 0;
+    if (next !== null) {
+      result = next;
+    } else if (total !== null) {
+      result = total;
+    }
     return (
-      <div className="container">
+      <div className="calc-container">
         <div className="row">
-          <span className="result text-end col-md-4 py-3">0</span>
+          <span className="result text-end col-md-4 py-3">{result}</span>
         </div>
         <div className="row">
-          <Button btnClass="gray-btn" input="A/C" />
-          <Button btnClass="gray-btn" input="+/-" />
-          <Button btnClass="gray-btn" input="%" />
-          <Button btnClass="orange-btn" input="÷" />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="AC" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="+/-" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="%" handleClickButton={this.handleClickButton} />
+          <Button btnClass="orange-btn" total={total} next={next} operation={operation} input="÷" handleClickButton={this.handleClickButton} />
         </div>
         <div className="row">
-          <Button btnClass="gray-btn" input={7} />
-          <Button btnClass="gray-btn" input={8} />
-          <Button btnClass="gray-btn" input={9} />
-          <Button btnClass="orange-btn" input="×" />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="7" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="8" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="9" handleClickButton={this.handleClickButton} />
+          <Button btnClass="orange-btn" total={total} next={next} operation={operation} input="x" handleClickButton={this.handleClickButton} />
         </div>
         <div className="row">
-          <Button btnClass="gray-btn" input={4} />
-          <Button btnClass="gray-btn" input={5} />
-          <Button btnClass="gray-btn" input={6} />
-          <Button btnClass="orange-btn" input="-" />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="4" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="5" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="6" handleClickButton={this.handleClickButton} />
+          <Button btnClass="orange-btn" total={total} next={next} operation={operation} input="-" handleClickButton={this.handleClickButton} />
         </div>
         <div className="row">
-          <Button btnClass="gray-btn" input={1} />
-          <Button btnClass="gray-btn" input={2} />
-          <Button btnClass="gray-btn" input={3} />
-          <Button btnClass="orange-btn" input="+" />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="1" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="2" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="3" handleClickButton={this.handleClickButton} />
+          <Button btnClass="orange-btn" total={total} next={next} operation={operation} input="+" handleClickButton={this.handleClickButton} />
         </div>
         <div className="row">
-          <Button btnClass="gray-btn col-md-2" input={0} />
-          <Button btnClass="gray-btn" input="." />
-          <Button btnClass="orange-btn" input="=" />
+          <Button btnClass="gray-btn col-md-2" total={total} next={next} operation={operation} input="0" handleClickButton={this.handleClickButton} />
+          <Button btnClass="gray-btn" total={total} next={next} operation={operation} input="." handleClickButton={this.handleClickButton} />
+          <Button btnClass="orange-btn" total={total} next={next} operation={operation} input="=" handleClickButton={this.handleClickButton} />
         </div>
       </div>
     );
